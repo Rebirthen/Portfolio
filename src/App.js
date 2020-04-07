bee0653feb185dc99750c872999064676a1b8b49
diana <rebirthed.flame@gmail.com>
2020-04-07 15:21:15 +0600

    edit homepage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const sections = [
 
 function App() {
   return (
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter basename="/Portfolio">
       <div className="App">
         <Header title="Diana Temirkhan" sections={sections} />
         <Route exact path="/" component={Work} />
